5f70190e6b2e682ca7b025fa2cceb2cef69fd3ed
Dominika Adamska <adamskdominik@gmail.com>
2023-01-05 14:05:53 +0100

    details extracted from ScentFamily component

--- components/scentFamily/ScentFamily.tsx
+++ components/scentFamily/ScentFamily.tsx
@@ -1,12 +1,8 @@
-import React, { createContext, Suspense, useState } from "react"
-import styles from './scentFamily.module.css'
+import React from "react"
 import { Scent } from '../scent/Scent'
-import { Carousel } from '../scentCarousel/Carousel'
-import { BasicScentsChoice } from './BasicScentsChoice'
+import { ScentFamilyDecorator } from "./scentFamilyDecorator"
 import { useAppSelector } from '../../app/hooks';
 import { selectSearch } from "../../components/search/searchSlice";
-
-export const ScentFamilyContext = createContext(false);
 
 interface IScentFamilyProps {
     scentFamilyName: string
@@ -19,55 +15,39 @@ export const ScentFamily = ({
     note,
     scents,
 }: IScentFamilyProps) => {
-    const [showBasicScents, setShowBasicScent] = useState(false)
-    const scentFamilyContextValues = showBasicScents
-    const selectPhrase = useAppSelector(selectSearch);
+    const searchedPhrase = useAppSelector(selectSearch);
     let results = [] 
- 
-
-    const toggleBasicScents = () => {
-        setShowBasicScent(prevShowBasicScents => !prevShowBasicScents)
-    }
 
     const filterScents = () => {
         const filteredScents = scents.filter(scent => {
-            const scentPopularName = scent.name.toLowerCase().includes(selectPhrase.toLowerCase())
-            const scentLatinName = scent.latinName.toLowerCase().includes(selectPhrase.toLowerCase())
+            const scentPopularName = scent.name.toLowerCase().includes(searchedPhrase.toLowerCase())
+            const scentLatinName = scent.latinName.toLowerCase().includes(searchedPhrase.toLowerCase())
             return scentPopularName || scentLatinName
         })
         return filteredScents
     }
 
-    results = selectPhrase === '' ?  scents : filterScents()
-    let isOpen = results.length > 0 ? true : false;  
+    results = searchedPhrase === '' ?  scents : filterScents()
 
     return (
-        <ScentFamilyContext.Provider value = { scentFamilyContextValues }>
-            <details open = { isOpen } className = { styles.details }>
-                <summary className = { styles.summary }>
-                    <div>
-                        <h2>{ scentFamilyName }</h2>
-                        <BasicScentsChoice toggleBasicScents = { toggleBasicScents }/>
-                    </div>
-                </summary>
-                <Suspense fallback = {<p>Loading...</p>}>
-                    <Carousel>
-                        {
-                            results.map(scent => {
-                                return(
-                                    <li key = { scent.scentId } >
-                                        <Scent 
-                                            label = { scent.name }
-                                            latinName = { scent.latinName }
-                                            liquidColor = { scent.color }
-                                            isBasic = { scent.isBasic } />  
-                                    </li>
-                                )
-                            })
-                        }
-                    </Carousel>
-                </Suspense>
-            </details>
-        </ScentFamilyContext.Provider>
+        <ScentFamilyDecorator
+            scentFamilyName = { scentFamilyName }
+            resultsLength = { results.length }
+        >
+            {
+                results.map(scent => {
+                    return(
+                        <li key = { scent.scentId } >
+                            <Scent 
+                                label = { scent.name }
+                                latinName = { scent.latinName }
+                                liquidColor = { scent.color }
+                                isBasic = { scent.isBasic }
+                            />  
+                        </li>
+                    )
+                })
+            }
+        </ScentFamilyDecorator>
     )
 }
